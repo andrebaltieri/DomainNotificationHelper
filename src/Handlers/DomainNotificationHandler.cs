4eabb6e19929e35b90ea80dd3fd6ee4d21101472
Andre Baltieri <andrebaltieri@hotmail.com>
2015-08-31 21:49:50 -0300

    Nova versão

---
+++ src/Handlers/DomainNotificationHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using DomainNotificationHelper.Events;
+
+namespace DomainNotificationHelper.Handlers
+{
+    public class DomainNotificationHandler : IHandler<DomainNotification>
+    {
+        private List<DomainNotification> _notifications;
+
+        public DomainNotificationHandler()
+        {
+            _notifications = new List<DomainNotification>();
+        }
+
+        public void Handle(DomainNotification args)
+        {
+            _notifications.Add(args);
+        }
+
+        public IEnumerable<DomainNotification> Notify()
+        {
+            return GetValue();
+        }
+
+        public bool HasNotifications()
+        {
+            return GetValue().Count > 0;
+        }
+
+        public void Dispose()
+        {
+            _notifications = new List<DomainNotification>();
+        }
+
+        private List<DomainNotification> GetValue()
+        {
+            return _notifications;
+        }
+    }
+}
